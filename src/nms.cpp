d046ecf3096375ba44d83bb07d44f582c3cf9c0c
Muhammad Zeeshan <muhammadzeeshan5784@gmail.com>
2026-01-05 17:47:59 +0500

    Update nms.cpp

--- src/nms.cpp
+++ src/nms.cpp
@@ -230,7 +230,7 @@ main()
 
     // Root Instructions
     SetConsoleTitleA("NASA HORIZON - PROJECT TITAN");
-    setCursor(true);
+    setCursor(false);
     srand(time(0)); // Seed RNG
     system("cls");
     cout << RD << "Please Wait! Initializing the Boot";
